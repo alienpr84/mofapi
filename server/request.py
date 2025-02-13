37157fda60ca3ee9bbdd85d0674ff625a169384c
Alien Padilla Rodriguez <alienpr84@gmail.com>
2025-02-12 23:55:27 -0300

    Server improvement and camel case notation

--- server/request.py
+++ server/request.py
@@ -2,27 +2,27 @@ import json
 from urllib.parse import urlparse, parse_qs
 
 class Request:
-  def __init__(self, raw_data):
-    lines = raw_data.split("\r\n")
-    request_line = lines[0].split(" ")
+  def __init__(self, rawData):
+    lines = rawData.split("\r\n")
+    requestLine = lines[0].split(" ")
 
-    self.method = request_line[0]
-    self.path = urlparse(request_line[1]).path
-    self.queryParams = parse_qs(urlparse(request_line[1]).query)
+    self.method = requestLine[0]
+    self.path = urlparse(requestLine[1]).path
+    self.queryParams = parse_qs(urlparse(requestLine[1]).query)
     self.headers = {}
 
-    body_index = 0
+    bodyIndex = 0
     for i, line in enumerate(lines[1:]):
       if line == "":
-        body_index = i + 2
+        bodyIndex = i + 2
         break
       key, value = line.split(": ", 1)
       self.headers[key] = value
     
-    raw_body = "\r\n".join(lines[body_index:])
+    raw_body = "\r\n".join(lines[bodyIndex:])
     try:
       self.body = json.loads(raw_body) if raw_body else None
     except json.JSONDecodeError:
       self.body = raw_body
       
-    print(f'method: {self.method}, path: {self.path}, queryParams: {self.queryParams}, headers: {self.headers}, body: {self.body}')
+    # print(f'method: {self.method}, path: {self.path}, queryParams: {self.queryParams}, headers: {self.headers}, body: {self.body}')
--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 from .request import Request
 from .response import Response
 import socket
+from socket import socket as Socket
 import threading
 from .router import Router
 import select
@@ -8,46 +9,41 @@ import select
 class Server:
   def __init__(self, router: Router, host: str = '127.0.0.1'):
     self.host = host
-    self.port = 8000
-    self.listenMessage = f"Server running on {self.host}:{self.port}... (Press Ctrl+C to stop)"
     self.router = router
 
 
-  def handle_client(self, client_socket):
+  def handleClient(self, clientSocket: Socket) -> None:
     try:
-      raw_request = client_socket.recv(1024).decode()
-      request = Request(raw_request)
-
-      handler = self.router.find_handler(request.method, request.path)
+      rawRequest = clientSocket.recv(1024).decode()
+      request = Request(rawRequest)
+      handler = self.router.findHandler(request.method, request.path)
       response = handler(request) if handler else Response("Not Found", 404)
-
-      client_socket.sendall(response.to_http_response().encode())
+      clientSocket.sendall(response.to_http_response().encode())
     finally:
-        client_socket.close()
+        clientSocket.close()
 
-  def listen(self, port: int = None, msg: str = None):
-    self.port = port if port is not None else self.port
-    self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    self.server_socket.bind((self.host, self.port))
-    self.server_socket.listen(5)
-    self.server_socket.setblocking(False)  # Set the server socket to non-blocking mode
-    self.running = True
-    self.listenMessage = msg if msg is not None else self.listenMessage
-    print(self.listenMessage)
+  def listen(self, port: int = None, msg: str = None) -> None:
+    port = port if port is not None else 8000
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serverSocket.bind((self.host, port))
+    serverSocket.listen(5)
+    serverSocket.setblocking(False)  # Set the server socket to non-blocking mode
+    isRunning = True
+    listenMessage = msg if msg is not None else f"Server running on {self.host}:{port}... (Press Ctrl+C to stop)"
+    print(listenMessage)
 
     try:
-      while self.running:
+      while isRunning:
         # Use select to handle socket readiness and avoid blocking forever
-        readable, _, _ = select.select([self.server_socket], [], [], 1)
+        readable, _, _ = select.select([serverSocket], [], [], 1)
 
         if readable:
-            client_socket, _ = self.server_socket.accept()
-            client_thread = threading.Thread(target=self.handle_client, args=(client_socket, ))
-            client_thread.start()
-
+          clientSocket, _ = serverSocket.accept()
+          clientThread = threading.Thread(target=self.handleClient, args=(clientSocket, ))
+          clientThread.start()
     except KeyboardInterrupt:
         print("\nServer shutting down gracefully...")
-        self.running = False
+        isRunning = False
     finally:
-        self.server_socket.close()
+        serverSocket.close()
